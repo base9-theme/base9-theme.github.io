b558502cda11671189ecb4f143bf9d4b92c465bd
Jiaqi Li <lijiaqigreat@gmail.com>
2022-02-10 06:52:53 +0000

    co

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
     'airbnb',
   ],
-  'parserOptions': {
-    'ecmaVersion': 13,
-    'parser': '@typescript-eslint/parser',
-    'sourceType': 'module',
+  parserOptions: {
+    ecmaVersion: 13,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
   },
-  'plugins': [
+  plugins: [
     'vue',
     '@typescript-eslint',
   ],
-  'rules': {
+  rules: {
   },
 };
